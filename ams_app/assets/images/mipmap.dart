f48a6682627453454933e87f46a057e361aa6730
口十耳 <yshye1989@aliyun.com>
2019-03-22 00:15:45 +0800

    ams_img

---
+++ ams_app/assets/images/mipmap.dart
@@ -0,0 +1,208 @@
+class YeMipmap {
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/first.png)
+  final String first = 'assets/images/first.png';
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/logo_v9.png)
+  final String logoV9 = 'assets/images/logo_v9.png';
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_JJKH.png)
+  final String addJjkh = "assets/images/index_add/ADD_JJKH.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_JJKH_DK.png)
+  final String addJjkhDk = "assets/images/index_add/ADD_JJKH_DK.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_SP.png)
+  final String addSp = "assets/images/index_add/ADD_SP.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_TSCL.png)
+  final String addTscl = "assets/images/index_add/ADD_TSCL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_WXRW.png)
+  final String addWxrw = "assets/images/index_add/ADD_WXRW.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_ZSKH.png)
+  final String addZskh = "assets/images/index_add/ADD_ZSKH.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_ZSKH_JD.png)
+  final String addZskhJd = "assets/images/index_add/ADD_ZSKH_JD.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_add/ADD_ZSKH_QD.png)
+  final String addZskhQd = "assets/images/index_add/ADD_ZSKH_QD.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_AQXG.png)
+  final String moduleAqxg = "assets/images/index_module/MODULE_AQXG.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_AQXGKB.png)
+  final String moduleAqxgkb = "assets/images/index_module/MODULE_AQXGKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_AQXJ.png)
+  final String moduleAqxj = "assets/images/index_module/MODULE_AQXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_AQXJKB.png)
+  final String moduleAqxjkb = "assets/images/index_module/MODULE_AQXJKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_BJXJ.png)
+  final String moduleBjxj = "assets/images/index_module/MODULE_BJXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_BJXJKB.png)
+  final String moduleBjxjkb = "assets/images/index_module/MODULE_BJXJKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_BZSBXJ.png)
+  final String moduleBzsbxj = "assets/images/index_module/MODULE_BZSBXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_CCGL.png)
+  final String moduleCcgl = "assets/images/index_module/MODULE_CCGL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_CCKB.png)
+  final String moduleCckb = "assets/images/index_module/MODULE_CCKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_CKZL.png)
+  final String moduleCkzl = "assets/images/index_module/MODULE_CKZL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_CLZL.png)
+  final String moduleClzl = "assets/images/index_module/MODULE_CLZL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_CW.png)
+  final String moduleCw = "assets/images/index_module/MODULE_CW.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_FTB.png)
+  final String moduleFtb = "assets/images/index_module/MODULE_FTB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_GYS.png)
+  final String moduleGys = "assets/images/index_module/MODULE_GYS.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_GZRZ.png)
+  final String moduleGzrz = "assets/images/index_module/MODULE_GZRZ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_JJKH.png)
+  final String moduleJjkh = "assets/images/index_module/MODULE_JJKH.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_JYZB.png)
+  final String moduleJyzb = "assets/images/index_module/MODULE_JYZB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_KFLXD.png)
+  final String moduleKflxd = "assets/images/index_module/MODULE_KFLXD.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_KH.png)
+  final String moduleKh = "assets/images/index_module/MODULE_KH.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_KHSJ.png)
+  final String moduleKhsj = "assets/images/index_module/MODULE_KHSJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_KQKB.png)
+  final String moduleKqkb = "assets/images/index_module/MODULE_KQKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_MORE.png)
+  final String moduleMore = "assets/images/index_module/MODULE_MORE.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_NHCB.png)
+  final String moduleNhcb = "assets/images/index_module/MODULE_NHCB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_NHKB.png)
+  final String moduleNhkb = "assets/images/index_module/MODULE_NHKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_RCGL.png)
+  final String moduleRcgl = "assets/images/index_module/MODULE_RCGL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SBBY.png)
+  final String moduleSbby = "assets/images/index_module/MODULE_SBBY.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SBWX.png)
+  final String moduleSbwx = "assets/images/index_module/MODULE_SBWX.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SBXJKB.png)
+  final String moduleSbxjkb = "assets/images/index_module/MODULE_SBXJKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SBYX.png)
+  final String moduleSbyx = "assets/images/index_module/MODULE_SBYX.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SBZL.png)
+  final String moduleSbzl = "assets/images/index_module/MODULE_SBZL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SHXJ.png)
+  final String moduleShxj = "assets/images/index_module/MODULE_SHXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SHXJKB.png)
+  final String moduleShxjkb = "assets/images/index_module/MODULE_SHXJKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SP.png)
+  final String moduleSp = "assets/images/index_module/MODULE_SP.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_SPRW.png)
+  final String moduleSprw = "assets/images/index_module/MODULE_SPRW.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_TS.png)
+  final String moduleTs = "assets/images/index_module/MODULE_TS.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_TSCL.png)
+  final String moduleTscl = "assets/images/index_module/MODULE_TSCL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_WXRW.png)
+  final String moduleWxrw = "assets/images/index_module/MODULE_WXRW.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_XFXJ.png)
+  final String moduleXfxj = "assets/images/index_module/MODULE_XFXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_XFXJKB.png)
+  final String moduleXfxjkb = "assets/images/index_module/MODULE_XFXJKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_XJD.png)
+  final String moduleXjd = "assets/images/index_module/MODULE_XJD.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_YHZL.png)
+  final String moduleYhzl = "assets/images/index_module/MODULE_YHZL.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_YY.png)
+  final String moduleYy = "assets/images/index_module/MODULE_YY.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZKB.png)
+  final String moduleZkb = "assets/images/index_module/MODULE_ZKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZSK.png)
+  final String moduleZsk = "assets/images/index_module/MODULE_ZSK.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZSKH.png)
+  final String moduleZskh = "assets/images/index_module/MODULE_ZSKH.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZSXM.png)
+  final String moduleZsxm = "assets/images/index_module/MODULE_ZSXM.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZX.png)
+  final String moduleZx = "assets/images/index_module/MODULE_ZX.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZXDA.png)
+  final String moduleZxda = "assets/images/index_module/MODULE_ZXDA.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZXXJ.png)
+  final String moduleZxxj = "assets/images/index_module/MODULE_ZXXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZXXJKB.png)
+  final String moduleZxxjkb = "assets/images/index_module/MODULE_ZXXJKB.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZXZG.png)
+  final String moduleZxzg = "assets/images/index_module/MODULE_ZXZG.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/index_module/MODULE_ZYSBXJ.png)
+  final String moduleZysbxj = "assets/images/index_module/MODULE_ZYSBXJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_DK.png)
+  final String mineDk = "assets/images/mine/MINE_DK.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_GJ.png)
+  final String mineGj = "assets/images/mine/MINE_GJ.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_KFLXD.png)
+  final String mineKflxd = "assets/images/mine/MINE_KFLXD.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_SC.png)
+  final String mineSc = "assets/images/mine/MINE_SC.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_TS.png)
+  final String mineTs = "assets/images/mine/MINE_TS.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_WX.png)
+  final String mineWx = "assets/images/mine/MINE_WX.png";
+
+  /// ![](https://raw.githubusercontent.com/yshye/imgs/master/ams_app/assets/images/mine/MINE_YJFK.png)
+  final String mineYjfk = "assets/images/mine/MINE_YJFK.png";
+}
